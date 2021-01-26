d46718a93249d838793593f78af896a3f1f7eeca
Dmitry <dima.klimov.98@inbox.ru>
2021-01-26 15:23:51 +0300

    init commit

---
+++ src/main/java/home/klimov/testtaskspringdbpostgres/TestTaskSpringDbPostgresApplication.java
@@ -0,0 +1,13 @@
+package home.klimov.testtaskspringdbpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSpringDbPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskSpringDbPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/klimov/testtaskspringdbpostgres/TestTaskSpringDbPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package home.klimov.testtaskspringdbpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskSpringDbPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
